fd27fbd0623b39d6d3f664cc4e42d1172efd7e00
AK <kkucenko.aleksejs@gmail.com>
2022-10-19 15:41:04 +0300

    Chore | Initialize prisma edge

--- src/service/prisma/prisma.ts
+++ src/service/prisma/prisma.ts
@@ -6,7 +6,7 @@ declare global {
    var prisma: PrismaClient | undefined
 }
 
-export const prisma: PrismaClient =
+export const prisma =
    global.prisma ||
    new PrismaClient({ log: ["query"], })
 
