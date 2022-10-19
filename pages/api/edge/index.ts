86adcce6364a94a8721ee29d7fa5cb6f3a5dd807
AK <kkucenko.aleksejs@gmail.com>
2022-10-19 15:44:24 +0300

    Chore | Remove prisma from edge route

--- pages/api/edge/index.ts
+++ pages/api/edge/index.ts
@@ -1,38 +1,40 @@
-import { prisma } from "@service/prisma"
+// import { prisma } from "@service/prisma"
 
 export const config = { runtime: "experimental-edge", }
 
 export default async function handler(): Promise<Response> {
+   
+   return new Response("Seems ok!", { status: 200 })
 
-   try {
-      const users = await prisma.user.findMany({
-         select: {
-            id: true,
-            email: true,
-            name: true,
-            role: true,
-            imageUrl: true,
-            createdAt: true,
-            updatedAt: true
-         },
-         orderBy: { createdAt: "desc" }
-      })
-
-      return new Response(
-         JSON.stringify(users),
-         {
-            status: 200,
-            headers: { "content-type": "application/json", },
-         }
-      )
-
-   } catch (err) {
-      return new Response(
-         JSON.stringify({ message: err.message }),
-         {
-            status: 400,
-            headers: { "content-type": "application/json", },
-         }
-      )
-   }
+   // try {
+   //    const users = await prisma.user.findMany({
+   //       select: {
+   //          id: true,
+   //          email: true,
+   //          name: true,
+   //          role: true,
+   //          imageUrl: true,
+   //          createdAt: true,
+   //          updatedAt: true
+   //       },
+   //       orderBy: { createdAt: "desc" }
+   //    })
+   //
+   //    return new Response(
+   //       JSON.stringify(users),
+   //       {
+   //          status: 200,
+   //          headers: { "content-type": "application/json", },
+   //       }
+   //    )
+   //
+   // } catch (err) {
+   //    return new Response(
+   //       JSON.stringify({ message: err.message }),
+   //       {
+   //          status: 400,
+   //          headers: { "content-type": "application/json", },
+   //       }
+   //    )
+   // }
 }
